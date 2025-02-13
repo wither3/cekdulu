cddd19deab0793fe03afad483f9ac252c3ec1215
wither3 <84785651+wither3@users.noreply.github.com>
2025-02-13 19:37:44 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+const express = require("express");
+const axios = require("axios");
+const { ytmp3a } = require("./api/ytmp3");
 
+const app = express();
+
+const FIREBASE_URL = "https://tempatrahasia-201bd-default-rtdb.asia-southeast1.firebasedatabase.app/data.json";
+
+// Route untuk mengambil data dari Firebase
+app.get("/cek/dataku", async (req, res) => {
+  try {
+    const response = await axios.get(FIREBASE_URL);
+    res.setHeader("Cache-Control", "no-store, max-age=0");
+    res.status(200).json(response.data);
+  } catch (error) {
+    res.status(500).json({ error: "Gagal mengambil data JSON" });
+  }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server berjalan di http://localhost:${PORT}`);
+});
